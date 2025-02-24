b609bf152d58ef943d80a27e29b5ad71b08052d7
READYonHub <readyemail3@gmail.com>
2025-02-24 17:14:48 +0100

    3. router and authentucation

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 * npm install -f imagekitio-react
 * npm i -f react-router-dom
+* npm i -f @clerk/clerk-react
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,61 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './routes/Homepage.jsx'
+import PostListPage from './routes/PostListPage.jsx'
+import LoginPage from './routes/LoginPage.jsx'
+import RegisterPage from './routes/RegisterPage.jsx'
+import SinglePostPage from './routes/SinglePostPage.jsx'
+import MainLayout from './layouts/MainLayout.jsx'
+import WritePage from './routes/WritePage.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/posts",
+        element: <PostListPage />,
+      },
+      {
+        path: "/:slug",
+        element: <SinglePostPage />,
+      },
+      {
+        path: "/write",
+        element: <WritePage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}   >
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>,
 )
