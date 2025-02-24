b7f8440f2e0b9024229f7e81314dddb559b5c37e
READYonHub <readyemail3@gmail.com>
2025-02-24 14:10:08 +0100

    2. hamburger menu 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,14 +11,32 @@ const Navbar = () => {
                 <img src="/logo.png" className="w-8 h-8" alt="" />
                 <span>ZABABLOG</span>
             </div>
-            {/* MOBILE MENU */}
+            {/* MOBILE Button */}
             <div className="md:hidden">
-                <div className='cursor-pointer text-4xl' onClick={() =>setOpen((prev) => !prev)}>
+                <div className='cursor-pointer text-4xl' onClick={() => setOpen((prev) => !prev)}>
                     {open ? "X" : "☰"}
+                </div>
+                {/* Mobile link list*/}
+                <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium  text-lg absolute top-16 bg-red-700 transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"}`}>
+                    <a href="/">Home</a>
+                    <a href="/">Trending</a>
+                    <a href="/">Most Popular</a>
+                    <a href="/">About</a>
+                    <a href="/">
+                        <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login 👌</button>
+                    </a>
                 </div>
             </div>
             {/* DESKTOP MENU */}
-            <div className="hidden md:flex ">D</div>
+            <div className="hidden md:flex items-center gap-8 xl:gap12 font-medium">
+                <a href="/">Home</a>
+                <a href="/">Trending</a>
+                <a href="/">Most Popular</a>
+                <a href="/">About</a>
+                <a href="/">
+                    <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login 👌</button>
+                </a>
+            </div>
         </div>
     )
 }
