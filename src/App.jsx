1ec232d836ad15cecb21a3d22b2bbe2c9b1ebada
READYonHub <readyemail3@gmail.com>
2025-02-24 13:54:07 +0100

    2. hamburger menu 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
-  return (
-    <div className=''>Hello World</div>
-  )
+    return (
+        <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px64">
+            {/*Navbar */}
+            <Navbar />
+            {/*Breadcrumb */}
+            {/*introduction */}
+            {/*featured posts*/}
+            {/*post list*/}
+        </div>)
 }
 
 export default App
